10075e013c4cf1dce5d03ba3bb3027b15ef61c78
Polina <borisovapolina5@gmail.com>
2021-12-17 21:38:52 +0200

    :rocket: Task_2_lms_6
    Add task_2 in lms_lesson_6

---
+++ lms_task_2_lesson6.py
@@ -0,0 +1,8 @@
+x = '0123456789'
+if x.isdigit() == True:
+    if len(x) == 10:
+        print("Okey, it's a correct number")
+    else:
+        print("You numer must be 10 characters long")
+else:
+    print("It's not a number!")
