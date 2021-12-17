fc6bdce20d7bf9ef5663f4ae72f6bf9cff51f7b2
Polina <borisovapolina5@gmail.com>
2021-12-17 20:03:12 +0200

    :fire: Changes in task_2
    Changed quotes in task_2

--- lms_task_2_print.py
+++ lms_task_2_print.py
@@ -1,16 +1,16 @@
 # 1,2,3
 print(1, 2, 3, sep=",")
 # Hi, my name is Polina
-print("Hi,", "my", "name", "is", "Polina", sep=" ")
+print('Hi,', 'my', 'name', 'is', 'Polina', sep=' ')
 # Its_me_again
-print('Its', 'me', 'again', sep="_")
+print('Its', 'me', 'again', sep='_')
 # 1 2 3
 print(1, 2, 3)
 # Hello, world!
-print('Hello, world', end="!\n")
+print('Hello, world', end='!\n')
 # Count to 3:
 print('Count to 3:', end='\n')
 # 1 2 3
 print(1, 2, 3, sep=" ", end='\n')
 # Hi!
-print("Hi!")
+print('Hi!')
