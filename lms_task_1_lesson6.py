a5e4545ca20b8d02422986df3cdc7ef1e90e52ce
Polina <borisovapolina5@gmail.com>
2021-12-20 20:36:59 +0200

    :tada: Task_3_lms_6
    Changed task_3 in lms_lesson_6

--- lms_task_1_lesson6.py
+++ lms_task_1_lesson6.py
@@ -1,6 +1,14 @@
-sample_string = input('write something')
-if len(sample_string) >= 2:
-    expected_string = sample_string[:2] + sample_string[-2:]
-else:
-    expected_string = ''
-print(expected_string)
+def string_manipulation(sample_string):
+    if len(sample_string) >= 2:
+        expected_string = sample_string[:2] + sample_string[-2:]
+    else:
+        expected_string = ''
+    return expected_string
+
+
+def string_input():
+    return input('write something: ')
+
+
+if __name__ == '__main__':
+    print(string_manipulation(string_input()))
