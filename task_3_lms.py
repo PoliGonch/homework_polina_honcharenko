4af66adb4260e0ed0e70a58cf8a10ccc93afcc10
Polina <borisovapolina5@gmail.com>
2021-12-21 19:11:28 +0200

    :tada: Task_3_lms_6
    Changed task_3 in lms_lesson_6

--- task_3_lms.py
+++ task_3_lms.py
@@ -1,8 +1,17 @@
-def name_check():
+def is_name_valid(name_from_storage: str, name_from_input: str) -> bool:
+    return name_from_storage == name_from_input.lower()
+
+
+def name_check() -> None:
     name = 'anton'
     user_name = input('Enter your name: ')
-    return name == user_name.lower()
+
+    is_equal = is_name_valid(name_from_storage=name, name_from_input=user_name)
+
+    massage_about_state = '' if is_equal else 'not'
+    massage = f'Name {name} is {massage_about_state} equal to name {user_name}'
+    print(massage)
 
 
 if __name__ == '__main__':
-    print(name_check())
+    name_check()
