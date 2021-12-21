08e1c0373da7b6e02e8b9a23f71587c91afb5140
Polina <borisovapolina5@gmail.com>
2021-12-21 19:26:51 +0200

    :sparkles: Task_2_lms_6
    Changed task_2 in lms_lesson_6

--- lms_task_2_lesson6.py
+++ lms_task_2_lesson6.py
@@ -1,8 +1,18 @@
-x = str('0123456789')
-if x.isdigit() == True:
-    if len(x) == 10:
-        print("Okey, it's a correct number")
+def valid_prone(phone_number: str) -> bool:
+    return phone_number.isdigit()
+
+
+def print_massage():
+    example_string = '0123456789'
+
+    if valid_prone(example_string):
+        if len(example_string) == 10:
+            print("Okey, it's a correct number")
+        else:
+            print("You numer must be 10 characters long")
     else:
-        print("You numer must be 10 characters long")
-else:
-    print("It's not a number!")
+        print("It's not a number!")
+
+
+if __name__ == '__main__':
+    print_massage()
