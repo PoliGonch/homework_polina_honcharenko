c1b68379f181d0af17cf3b5361a23291a378db6c
Polina <borisovapolina5@gmail.com>
2021-12-22 20:21:59 +0200

    :tada: Task_3_lms_5
    Changed task_3 in lms_lesson_5

--- lms_task_3_lesson5.py
+++ lms_task_3_lesson5.py
@@ -1,9 +1,18 @@
-a = 3
-b = 2
-print(a + b)
-print(a - b)
-print(a / b)
-print(a * b)
-print(a ** b)
-print(a % b)
-print(a // b)
+from typing import Union
+
+
+def operator(x: Union[float, int], y: float | int) -> dict[str, float]:
+    results = {''
+               'addition': x + y,
+               'substraction': x - y,
+               'division': x / y,
+               'multiplication': x * y,
+               'exponent': x ** y,
+               'Modulus': x % y,
+               'floor division': x // y,
+               }
+    return results
+
+
+if __name__ == '__main__':
+    print(operator(5, 2))
