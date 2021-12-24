a28b2ad3b9c41cd062dfc119e05ff19645cd03f8
Polina <borisovapolina5@gmail.com>
2021-12-24 22:15:36 +0200

    :tada: Task_3_lms_5
    Changed task_3 in lms_lesson_5

--- lms_task_3_lesson5.py
+++ lms_task_3_lesson5.py
@@ -1,18 +1,26 @@
-from typing import Union
+from typing import Union, TypeAlias
+
+T_NUMBER: TypeAlias = int | float
+T_RESULT: TypeAlias = dict[str, T_NUMBER]
 
 
-def operator(x: Union[float, int], y: float | int) -> dict[str, float]:
-    results = {''
-               'addition': x + y,
-               'substraction': x - y,
-               'division': x / y,
-               'multiplication': x * y,
-               'exponent': x ** y,
-               'Modulus': x % y,
-               'floor division': x // y,
-               }
+def operator(x: Union[float, int], y: T_NUMBER) -> T_RESULT:
+    results = {
+        'addition': x + y,
+        'substraction': x - y,
+        'division': x / y,
+        'multiplication': x * y,
+        'exponent': x ** y,
+        'Modulus': x % y,
+        'floor division': x // y,
+    }
     return results
 
 
+def dict_print(data: T_RESULT) -> None:
+    for operation, result in data.items():
+        print(f'Result for {operation} is {result}')
+
+
 if __name__ == '__main__':
-    print(operator(5, 2))
+    dict_print(operator(5, 2))
