921a19e5ac740af7a11102a37c517c54754bb3fb
Polina <borisovapolina5@gmail.com>
2022-01-04 12:27:03 +0200

    :rocket: Task_4
    Task_4 in lesson_007

---
+++ task_4_lms.py
@@ -0,0 +1,16 @@
+def check_answer(result: int, answer: int):
+    if result == answer:
+        print('This is correct!')
+    else:
+        print('Try again')
+
+
+def main():
+    math_expression = '3+2'
+    result = eval(math_expression)
+    answer = int(input(f'What is your answear for {math_expression}? Put it here:\n'))
+    check_answer(result=result, answer=answer)
+
+
+if __name__ == '__main__':
+    main()
