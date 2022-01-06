385e7cb64053ec9941ca35eddfe36df354a56171
Polina <borisovapolina5@gmail.com>
2022-01-06 13:27:05 +0200

    :tada: Task_1
    Add Task_1 to lesson_009

--- task_1_lms.py
+++ task_1_lms.py
@@ -1,15 +1,14 @@
 import random
 
-
-def greatest_number(list: list) -> int:
-    return max(list)
+def greatest_number(num_list: list) -> int:
+    return max(num_list)
 
 
 def main() -> None:
     numbers_list = []
     while len(numbers_list) < 10:
         numbers_list.append(random.randint(1, 100))
-    greatest_number(numbers_list)
+    greatest_number(num_list=numbers_list)
 
 
 if __name__ == '__main__':
