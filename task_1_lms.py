3ea338327e6800c353644a8a1366d2293c448a52
Polina <borisovapolina5@gmail.com>
2022-01-11 18:44:40 +0200

    :tada: Add task_1_lms
    Add Tsk_1 to lesson_012

---
+++ task_1_lms.py
@@ -0,0 +1,17 @@
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def talk(self) -> None:
+        print(f'Hello, my name is {self.first_name} {self.last_name} and I\'m {self.age} years old.')
+
+
+polina = Person('Polina', 'Goncharenko', 24)
+carl = Person('Carl', 'Johnson', 26)
+
+if __name__ == '__main__':
+    polina.talk()
+    carl.talk()
