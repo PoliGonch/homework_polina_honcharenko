6206d4a1c303880f4d5358d976472f498dcc9ab2
Polina <borisovapolina5@gmail.com>
2022-01-30 21:42:51 +0200

    :tada: Task_4_lms Add task_4 to lms_lesson_13

--- task_4_lms.py
+++ task_4_lms.py
@@ -1,13 +1,13 @@
 class CustomException(Exception):
 
     def __init__(self, msg):
-        self.msg = msg
+        self.msg = str(msg)
         with open('logs.txt', 'a') as log_file:
             log_file.write(f'{self.msg}\n')
 
 
 def main():
-    raise CustomException("hello")
+    raise CustomException("test exc")
 
 
 if __name__ == '__main__':
