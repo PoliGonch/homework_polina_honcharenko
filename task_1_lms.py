5d723680d44b566787c2141dd738396b636eb9f6
Polina <borisovapolina5@gmail.com>
2022-02-05 13:19:09 +0200

    :tada: task_1_lms Add task_1 to lms_lesson_025

---
+++ task_1_lms.py
@@ -0,0 +1,22 @@
+# from typing import Optional
+
+def to_power(x, exp: int):
+    if exp == 1:
+        return x
+    return x * to_power(x, exp - 1)
+
+
+print(to_power(3, 4))
+
+"""
+    Returns  x ^ exp
+
+    >>> to_power(2, 3) == 8
+    True
+
+    >>> to_power(3.5, 2) == 12.25
+    True
+
+    >>> to_power(2, -1)
+    ValueError: This function works only with exp > 0.
+    """
