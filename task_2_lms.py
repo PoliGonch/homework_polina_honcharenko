16e5e3bc4aa2e9ce08c884e4658a9232ddebad60
Polina <borisovapolina5@gmail.com>
2022-02-09 19:30:27 +0200

    :tada: task_2 in 028 lesson lms Add task_2 to lms_lesson_028

--- task_2_lms.py
+++ task_2_lms.py
@@ -1,77 +1,32 @@
-from typing import TypeAlias
+def fibonacci_search(lys, val):
+    fib_minus_2 = 0
+    fib_minus_1 = 1
+    fib = fib_minus_1 + fib_minus_2
 
-from node import Node
+    while fib < len(lys):
+        fib_minus_2 = fib_minus_1
+        fib_minus_1 = fib
+        fib = fib_minus_1 + fib_minus_2
+    index = -1
 
-ItemType: TypeAlias = int
+    while fib > 1:
+        i = min(index + fib_minus_2, (len(lys) - 1))
 
-
-class UnorderedList:
-
-    def __init__(self):
-        self._head = None
-
-    def is_empty(self) -> bool:
-        return self._head is None
-
-    def size(self) -> int:
-        current = self._head
-        count = 0
-        while current is not None:
-            count += 1
-            current = current.get_next()
-        return count
-
-    def push(self, item: ItemType) -> bool:
-        temp = Node(item)
-        previous = None
-
-        if self._head is None:
-            self._head = temp
-
-        current = self._head
-        while current is not None:
-            previous = current
-            current = current.get_next()
-        previous.set_next(temp)
-        temp.set_next(current)
-        return True
-
-    def pop(self) -> bool | str:
-        previous = None
-        current = self._head
-
-        if self._head is None:
-            return 'No items found'
-
-        while current.get_next():
-            previous = current
-            current = current.get_next()
-        if previous is None:
-            self._head = None
+        if lys[i] < val:
+            fib = fib_minus_1
+            fib_minus_1 = fib_minus_2
+            fib_minus_2 = fib - fib_minus_1
+            index = i
+        elif lys[i] > val:
+            fib = fib_minus_2
+            fib_minus_1 = fib_minus_1 - fib_minus_2
+            fib_minus_2 = fib - fib_minus_1
         else:
-            previous.set_next(None)
-        return True
+            return i
 
-    def __repr__(self):
-        representation = "<UnorderedList: "
-        current = self._head
-        while current is not None:
-            representation += f"{current.get_data()} "
-            current = current.get_next()
-        return representation + ">"
-
-    def __str__(self):
-        return self.__repr__()
+    if fib_minus_1 and index < (len(lys) - 1) and lys[index + 1] == val:
+        return index + 1
+    return -1
 
 
-if __name__ == "__main__":
-    my_list = UnorderedList()
-
-    my_list.push(1)
-    my_list.push(2)
-    my_list.push(3)
-    my_list.push(4)
-    my_list.push(5)
-    print(my_list)
-    my_list.pop()
-    print(my_list)
+print(fibonacci_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8))
